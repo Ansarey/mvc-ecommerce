fe84f8a296962c5619ded43efbf753d4a850eb11
Ansarey <148070783+Ansarey@users.noreply.github.com>
2024-07-02 15:41:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MashaAllah!!!</title>
+  <title>Hello World!!!</title>
   <?php 
     require "header.php"; 
     require_once "includes/class_autoloader.php";
